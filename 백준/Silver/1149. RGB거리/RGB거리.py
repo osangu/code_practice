2ba000a0995fa8eaf20a7bfcb63917bebadf11bc
osangu <osangu012@gmail.com>
2024-01-18 00:41:41 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,10 +1,13 @@
+from sys import stdin
+
+input = stdin.readline
+
 N = int(input())
 
 table = [
     list(map(lambda x: [int(x)], input().split()))
     for i in range(N)
 ]
-
 
 for i in range(1, N):
     for j in range(3):
@@ -13,19 +16,23 @@ for i in range(1, N):
                 table[i - 1][1][0],
                 table[i - 1][2][0]
             )
+            table[i][j][0] += add
+            continue
 
         elif j == 1:
             add = min(
                 table[i - 1][0][0],
                 table[i - 1][2][0]
             )
+            table[i][j][0] += add
+            continue
 
         elif j == 2:
             add = min(
                 table[i - 1][0][0],
                 table[i - 1][1][0]
             )
-
-        table[i][j][0] += add
+            table[i][j][0] += add
+            continue
 
 print(min(*table[N - 1])[0])
