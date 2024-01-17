fe8dc432249e9c453a2d97e430aaec5faaf3d0d7
osangu <osangu012@gmail.com>
2024-01-18 00:40:13 +0900

    [Silver I] Title: RGB거리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -6,10 +6,6 @@ table = [
 ]
 
 
-def get_answer(r, g, b):
-    return min(r[0], g[0], b[0])
-
-
 for i in range(1, N):
     for j in range(3):
         if j == 0:
@@ -17,20 +13,19 @@ for i in range(1, N):
                 table[i - 1][1][0],
                 table[i - 1][2][0]
             )
-            table[i][0][0] += add
 
         elif j == 1:
             add = min(
                 table[i - 1][0][0],
                 table[i - 1][2][0]
             )
-            table[i][1][0] += add
 
         elif j == 2:
             add = min(
                 table[i - 1][0][0],
                 table[i - 1][1][0]
             )
-            table[i][2][0] += add
-            
-print(get_answer(*table[N - 1]))
+
+        table[i][j][0] += add
+
+print(min(*table[N - 1])[0])
